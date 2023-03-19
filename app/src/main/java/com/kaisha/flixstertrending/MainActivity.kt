c3e2a045a69b72a0ad29033aeec6ff9cf69163d3
Thurman Reynolds <mikireynolds.swe@gmail.com>
2023-03-18 20:39:10 -0500

    fix bug with injecting api key into BuildConfig.API_KEY in MainActivity.kt

--- app/src/main/java/com/kaisha/flixstertrending/MainActivity.kt
+++ app/src/main/java/com/kaisha/flixstertrending/MainActivity.kt
@@ -2,6 +2,18 @@ package com.kaisha.flixstertrending
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.serialization.json.Json
+
+
+fun createJson() = Json {
+    isLenient = true
+    ignoreUnknownKeys = true
+    useAlternativeNames = false
+}
+
+private const val TAG = "MainActivity/"
+private const val SEARCH_API_KEY = BuildConfig.API_KEY
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
