3da11abea6230982911890d4e840c4caa1add4ea
Thurman Reynolds <mikireynolds.swe@gmail.com>
2023-03-18 21:02:14 -0500

    set up main view

--- app/src/main/java/com/kaisha/flixstertrending/MainActivity.kt
+++ app/src/main/java/com/kaisha/flixstertrending/MainActivity.kt
@@ -2,8 +2,17 @@ package com.kaisha.flixstertrending
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.serialization.json.Json
+import com.kaisha.flixstertrending.databinding.ActivityMainBinding
 
+
+private const val TAG = "MainActivity/"
+private const val SEARCH_API_KEY = BuildConfig.API_KEY
+private const val MOVIE_SEARCH_URL =
+    "https://api.themoviedb.org/3/trending/movie/week?api_key=${SEARCH_API_KEY}"
+private const val SHOW_SEARCH_URL =
+    "https://api.themoviedb.org/3/trending/tv/week?api_key=${SEARCH_API_KEY}"
 
 fun createJson() = Json {
     isLenient = true
@@ -11,13 +20,18 @@ fun createJson() = Json {
     useAlternativeNames = false
 }
 
-private const val TAG = "MainActivity/"
-private const val SEARCH_API_KEY = BuildConfig.API_KEY
-
-
 class MainActivity : AppCompatActivity() {
+    private lateinit var moviesRecyclerView: RecyclerView
+    private lateinit var showsRecyclerView: RecyclerView
+    private lateinit var binding: ActivityMainBinding
+    private val movies = mutableListOf<Media>()
+    private val shows = mutableListOf<Media>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // new way of setting up the layout instead of the traditional way of findingID
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
